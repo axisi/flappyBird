2807f23fe90546ee847014d198d92f12d5205da5
Macała Grzegorz <grzegorz.macala@cezpolska.pl>
2021-07-14 20:00:22 +0200

    Initial commit

---
+++ .idea/main.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 400;
+
+let spacePressed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let score = 0;
+let gamespeed = 2;
+let temp = canvas.height - 100;
+
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    //ctx.fillRect( 10, temp,50,50);
+    bird.update();
+    bird.draw();
+    frame+=1;
+    angle+=0.2 ;
+    requestAnimationFrame(animate);
+   // console.log(spacePressed)
+}
+animate()
+
+window.addEventListener("keydown", function (e) {
+    if (e.code=== 'Space'){
+        spacePressed = true;
+
+    }
+})
+window.addEventListener("keyup", function (e) {
+    if (e.code=== 'Space'){
+        spacePressed = false;
+
+    }
+})
